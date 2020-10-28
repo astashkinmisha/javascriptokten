6d3184829f368aba7705c0c0e2a6038e204f355c
astashkinmisha <71668690+astashkinmisha@users.noreply.github.com>
2020-10-28 17:00:53 +0200

    Add files via upload

---
+++ hw1.js
@@ -0,0 +1,265 @@
+ 1 task
+//let greeting = 'hello';
+//let school = 'owu';
+//let host  = "com";
+//let domain  = 'ua';
+//let year = 1;
+//let age = 10;
+//let minus = -999;
+//let count = 123;
+//let pi = 3.14;
+//let distance = 2.7;
+//let summ = 16;
+//let isAdult = false;
+//let isChild = true;
+//
+// 
+//
+//console.log(greeting) ;
+//alert(greeting);
+//document.write(greeting)
+//
+//console.log(school);
+//alert(school);
+//document.write(school);
+//
+//console.log(host);
+//alert(host);
+//document.write(host);
+//
+//console.log(domain);
+//alert(domain);
+//document.write(domain);
+//
+//console.log(year);
+//alert(year);
+//document.write(year);
+//
+//console.log(age);
+//alert(age);
+//document.write(age);
+//
+//console.log(minus);
+//alert(minus);
+//document.write(minus);
+//
+//console.log(count);
+//alert(count);
+//document.write(count);
+//
+//console.log(pi);
+//alert(pi);
+//document.write(pi);
+//
+//
+//console.log(distance);
+//alert(distance);
+//document.write(distance);
+//
+//console.log(summ);
+//alert(summ);
+//document.write(summ);
+//
+//console.log(isAdult);
+//alert(isAdult);
+//document.write(isAdult);
+//
+//console.log(isChild);
+//alert(isChild);
+//document.write(isChild);
+//
+
+2 task
+//greeting = 'bye';
+//
+//console.log(greeting);
+//alert(greeting);
+//document.write(greeting);
+//
+//school ='owu';
+//school ='qwert';
+//
+//console.log(school);
+//alert(school);
+//document.write(school);
+//
+//host  = "org";
+//
+//console.log(host);
+//alert(host);
+//document.write(host);
+//
+//domain  = 'sk';
+//
+//console.log(domain);
+//alert(domain);
+//document.write(domain);
+//
+//year = 5;
+//
+//console.log(year);
+//alert(year);
+//document.write(year);
+//
+//age = 23;
+//
+//console.log(age);
+//alert(age);
+//document.write(age);
+//
+//minus = -1;
+//
+//console.log(minus);
+//alert(minus);
+//document.write(minus);
+//
+//
+//count = 321;
+//
+//console.log(count);
+//alert(count);
+//document.write(count);
+//
+//pi = 4,13;
+//
+//console.log(pi);
+//alert(pi);
+//document.write(pi);
+//
+//
+//distance = 2.9;
+//console.log(distance);
+//alert(distance);
+//document.write(distance);
+//
+//summ = 8;
+//
+//console.log(summ);
+//alert(summ);
+//document.write(summ);
+//
+//isAdult = true;
+//
+//console.log(isAdult);
+//alert(isAdult);
+//document.write(isAdult);
+//
+//isChild = false;
+//console.log(isChild);
+//alert(isChild);
+//document.write(isChild);
+
+3 task
+
+//const srt ='3';
+//const string ='15';
+//const third ='third'
+//
+//const num = 3;
+//const numer = 17;
+//const numeric = 23;
+//
+//alert(srt);
+//alert(string);
+//alert(third);
+//alert(num);
+//alert(numer);
+//alert(numeric);
+//
+//
+//console.log(srt);
+//console.log(string);
+//console.log(third);
+//console.log(num);
+//console.log(numer);
+//console.log(numeric);
+
+ 4 task
+//document.write(srt);
+//document.write(string);
+//document.write(third);
+//document.write(num);
+//document.write(numer);
+//document.write(numeric);
+//l
+
+
+
+
+ task5
+
+ //let name ='misha';
+ //let surname ='astashkin';
+ //let secname ='mykhailovich';
+ //
+ //    name = prompt('Enter Name');
+ //
+ //    secname = prompt('Enter SecName');
+ //
+ //    surname = prompt('Enter Surname');
+ //
+ //    alert(name);
+ //    alert(secname);
+ //    alert(surname);
+ //
+ //    console.log(name);
+ //    console.log(secname);
+ //    console.log(surname);
+ //
+ //    document.write(name);
+ //    document.write(secname);
+ //    document.write(surname);
+ //let person = 'fullname';
+//person = name+secname+surname;
+//
+//console.log(person);
+
+
+
+task 7
+
+let number1 = prompt();
+let number2 = prompt();
+let number3 = prompt();
+number1 = +number1;
+number2 = +number2;
+number3 = +number3;
+console.log(number1, number2, number3);
+
+
+
+//task 8
+//
+//let first = parseInt(prompt("1"));
+//let second = parseInt(prompt("2"));
+//let third = parseInt(prompt("3"));
+//let fourth = parseInt(prompt("4"));
+//let result = first+second+third+fourth;
+//
+//
+//console.log(result);
+
+
+//task 
+//let first = parseFloat(prompt('1'));
+//let second = parseFloat(prompt('2'));
+//let third = parseFloat(prompt('3'));
+//
+//let summa = first+second+third;
+//console.log (summa);
+
+// task 14
+
+//console.log(5<6);
+//console.log(5>6);
+//console.log(5==6);
+//console.log(5>=6);
+//console.log(10==10);
+//console.log(10==10);
+//console.log(10<10);
+//console.log(10>10);
+//console.log(10!==10);
+//console.log(123 === '123');
+//console.log(123 !== '123');
+
+final
